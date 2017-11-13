404accdd0b22928d1c5b6e73bad22527f34cc852
Nishant Thakur <nt.nishantt@gmail.com>
2017-11-12 23:56:17 -0600

    Indentation

--- Level 2/foobar-Problem2(Attempt 1).py
+++ Level 2/foobar-Problem2(Attempt 1).py
@@ -1,6 +1,6 @@
 def answer(pegs):
     len_pegs = len(pegs)
-    if(len_pegs) == 1:
+    if (len_pegs) == 1:
         return [-1, -1]
     else:
         dist = []
@@ -8,35 +8,33 @@ def answer(pegs):
         min_dist = pegs[1] - pegs[0]
         min_index = 0
         for i in range(0, len_pegs - 1):
-            dist.append(pegs[i+1] - pegs[i])
+            dist.append(pegs[i + 1] - pegs[i])
             if dist[i] < min_dist:
                 min_dist = dist[i]
                 min_index = i
         # print (dist)
-        
+
         for radius in range(1, min_dist):
             current_index = min_index
-            #print (radius, current_index )
+            # print (radius, current_index )
             gear[current_index] = radius
             while current_index > 0:
                 gear[current_index - 1] = dist[current_index - 1] - gear[current_index]
                 current_index = current_index - 1
-                
+
             current_index = min_index
             while current_index < len_pegs - 1:
                 gear[current_index + 1] = dist[current_index] - gear[current_index]
                 current_index = current_index + 1
-                
+
             # print(gear)
-                
-            if gear[0] == gear[len_pegs-1]*2:
-              #   print([gear[0],1])
-                return [gear[0],1]
+
+            if gear[0] == gear[len_pegs - 1] * 2:
+                #   print([gear[0],1])
+                return [gear[0], 1]
                 break
         else:
-            return [-1,-1]
-                
-            
-        
-                
+            return [-1, -1]
+
+
 print(answer([4, 50]))
--- Level 2/foobar-Problem2(correct).py
+++ Level 2/foobar-Problem2(correct).py
@@ -1,31 +1,31 @@
 from fractions import Fraction
 
+
 def answer(pegs):
-    
     len_pegs = len(pegs)
-    
-    if  len_pegs < 2 or len_pegs > 20:
+
+    if len_pegs < 2 or len_pegs > 20:
         return [-1, -1]
-    
+
     r = 0
     dist = []
-    
-    #calculate the distance
+
+    # calculate the distance
     for i in range(0, len_pegs - 1):
-        dist.append(pegs[i+1] - pegs[i])
-        
-    #if number of pegs are odd
+        dist.append(pegs[i + 1] - pegs[i])
+
+    # if number of pegs are odd
     if len_pegs % 2 != 0:
         total = (- pegs[0] - pegs[len_pegs - 1])
     else:
-        #for even number of pegs
+        # for even number of pegs
         total = (- pegs[0] + pegs[len_pegs - 1])
-        
+
     if len_pegs > 2:
-        for i in range(1, len_pegs-1):
-            total += 2 * (-1)**(i+1) * pegs[i]
-            
-    r = Fraction(2 * (float(total)/3 if len_pegs % 2 == 0 else total)).limit_denominator()
+        for i in range(1, len_pegs - 1):
+            total += 2 * (-1) ** (i + 1) * pegs[i]
+
+    r = Fraction(2 * (float(total) / 3 if len_pegs % 2 == 0 else total)).limit_denominator()
 
     if r < 1:
         return [-1, -1]
@@ -40,10 +40,8 @@ def answer(pegs):
                 current_index = current_index + 1
             else:
                 return [-1, -1]
-        
-        return [r.numerator ,r.denominator]
-    
-    
-    
-                
-print(answer([4,30,50]))
+
+        return [r.numerator, r.denominator]
+
+
+print(answer([4, 30, 50]))
